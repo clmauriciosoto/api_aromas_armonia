79910ef270b8290f0c24b23bc043901095706ede
Mauricio Soto <cl.mauriciosoto@gmail.com>
2025-11-06 13:38:07 -0300

    Configure CORS to allow specific origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://aromasdearmonia.cl', 'https://www.aromasdearmonia.cl'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
