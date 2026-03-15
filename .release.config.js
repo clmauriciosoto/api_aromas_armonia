e25a975d3301773b9e80ef083f415db07e32de80
Mauricio Soto <cl.mauriciosoto@gmail.com>
2026-03-14 23:19:25 -0300

    feat: include chore commits in release notes

--- .release.config.js
+++ .release.config.js
@@ -1,8 +1,53 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'docs', release: false },
+          { type: 'refactor', release: false },
+          { type: 'build', release: false },
+          { type: 'ci', release: false },
+          { type: 'test', release: false },
+          { type: 'style', release: false },
+          { type: 'chore', release: false },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features', hidden: false },
+            { type: 'fix', section: 'Bug Fixes', hidden: false },
+            {
+              type: 'perf',
+              section: 'Performance Improvements',
+              hidden: false,
+            },
+            { type: 'refactor', section: 'Refactoring', hidden: false },
+            { type: 'docs', section: 'Documentation', hidden: false },
+            { type: 'build', section: 'Build System', hidden: false },
+            {
+              type: 'ci',
+              section: 'Continuous Integration',
+              hidden: false,
+            },
+            { type: 'test', section: 'Tests', hidden: false },
+            { type: 'style', section: 'Styles', hidden: false },
+            { type: 'chore', section: 'Chores', hidden: false },
+          ],
+        },
+      },
+    ],
     '@semantic-release/changelog',
     [
       '@semantic-release/npm',
